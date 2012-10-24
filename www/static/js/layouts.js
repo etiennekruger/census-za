fa10834eadab32c2361bde674e4210e508ab3406
Adi Eyal <adi@burgercom.co.za>
2012-10-24 13:11:54 +0200

    Refactored indicatorlist
    Indicator is a separate object that knows how to draw itself given a data object

--- www/static/js/layouts.js
+++ www/static/js/layouts.js
@@ -12,6 +12,28 @@ indicators = [
     { name : "Crime", url : "http://path.to.indicator" },
 ]
 
+/*
+    Object to draw a particular indicator
+*/
+IndicatorItem = function(data) {
+    this.data = data;
+}
+
+IndicatorItem.prototype = {
+    draw : function(rootElement) {
+        //rootElement.text(this.data.name);
+        var me = this;
+        rootElement.append("a")
+            .attr("href", function() {
+                return me.data.url;
+            })
+            .text(this.data.name);
+    }
+}
+
+/*
+    Widget to draw a list of indicators
+*/
 IndicatorList = function(ctx) {
     this.data = ctx.data; 
     this.rootElement = ctx.rootElement;
@@ -19,34 +41,41 @@ IndicatorList = function(ctx) {
     this.init();
 }
 
-IndicatorList.prototype  = {
+IndicatorList.prototype = {
 
     init : function() {
         var me = this;
+        // TODO this code might actually move out of here
+        // we could wire it up outside of this object
         d3.select("#searchBox")
             .on("keyup", function() {
                 var searchbox = this;
-                me.currentView = me.data.filter(function(el) {
-                    var str = searchbox.value.toLowerCase();
-                    return el.name.toLowerCase().indexOf(str) >= 0;
-                });
-                me.display(); 
+                var str = searchbox.value.toLowerCase();
+                me.filter(str);
             });
     },
+    filter : function(str) {
+        // Filter the displayed list of indicators
+        var me = this;
+        me.currentView = me.data.filter(function(el) {
+            return el.name.toLowerCase().indexOf(str) >= 0;
+        });
+        me.display(); 
+    },
     display : function() {
-    
+        // Display the updated list of indicators
         var me = this;
         d3.select(this.rootElement)
             .selectAll("li")
             .remove();
         d3.select(this.rootElement)
-           .selectAll("li")
-           .data(me.currentView)
-           .enter().append("li")
-           .text(function(d) {
-                return d.name;
-           }); 
-
+            .selectAll("li")
+            .data(me.currentView)
+            .enter().append("li")
+            .each(function(d) {
+                var item = new IndicatorItem(d);
+                item.draw(d3.select(this));
+            })
     }
 }
 
